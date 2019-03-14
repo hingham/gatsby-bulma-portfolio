f36df234e49edb062eed840bb85311ec1e39c0ca
Hannah Ingham <hannahingham@MacBooks-MacBook-Pro.local>
2019-03-14 13:15:29 -0700

    removing number

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,7 +10,7 @@ class Footer extends React.Component {
                 <strong> Contact: </strong>
             </div>
             <div className="has-text-centered">
-                hannah.c.ingham@gmail.com | 360-296-3167
+                hannah.c.ingham@gmail.com
             </div>
             <div className="has-text-centered"> {"\u00A9"} Hannah Ingham </div>
         </div>
